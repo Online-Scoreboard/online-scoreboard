26c197bab302cdf9914c971e10a6ad25bd2f55db
Kārlis Amoliņš <karlis.amolins.5@gmail.com>
2019-10-12 15:37:38 +0300

    config.yml: - added TS_NODE_PROJECT path - fixed report script typo - fixed artifact paths
    reporter.ts:
    - fixed screen shot path

--- e2e/reporting/reporter.ts
+++ e2e/reporting/reporter.ts
@@ -4,7 +4,7 @@ export const options = {
   theme: 'foundation',
   jsonFile: './e2e/reporting/json-report.json',
   output: './e2e/reporting/html-report.html',
-  screenshotsDirectory: './reporting',
+  screenshotsDirectory: './e2e/reporting',
   storeScreenshots: true,
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
