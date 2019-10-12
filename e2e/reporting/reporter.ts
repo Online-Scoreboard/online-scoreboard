06a171dd21aaee402d85998be29576d41b3737be
Kārlis Amoliņš <karlis.amolins.5@gmail.com>
2019-10-12 16:49:26 +0300

    Added: - placeholder .gitkeeps for storing folders for sorted reports
    Removed:
    - storing screenshots in reporter.ts

--- e2e/reporting/reporter.ts
+++ e2e/reporting/reporter.ts
@@ -4,8 +4,6 @@ export const options = {
   theme: 'foundation',
   jsonFile: './e2e/reporting/json/json-report.json',
   output: './e2e/reporting/html/html-report.html',
-  screenshotsDirectory: './e2e/reporting',
-  storeScreenshots: true,
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
   launchReport: false,
