3661ebefdc4f197913dcb96ea1da6fe6eae32de4
Kārlis Amoliņš <karlis.amolins.5@gmail.com>
2019-10-12 16:37:37 +0300

    Added TS_NODE_PROJECT env var to CCI config

--- e2e/reporting/reporter.ts
+++ e2e/reporting/reporter.ts
@@ -2,13 +2,13 @@ import { generate } from 'cucumber-html-reporter';
 
 export const options = {
   theme: 'foundation',
-  jsonFile: './e2e/reporting/json-report.json',
-  output: './e2e/reporting/html-report.html',
+  jsonFile: './e2e/reporting/json/json-report.json',
+  output: './e2e/reporting/html/html-report.html',
   screenshotsDirectory: './e2e/reporting',
   storeScreenshots: true,
   reportSuiteAsScenarios: true,
   scenarioTimestamp: true,
-  launchReport: true,
+  launchReport: false,
 };
 
 generate(options);
